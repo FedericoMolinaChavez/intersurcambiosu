d40c2a7eca24ae14018f7a7c63643776653ac77e
FedericoMolinaChavez <federicomolinachavez@gmail.com>
2017-02-18 22:52:25 -0500

    holi
    que hace

--- email_script.php
+++ email_script.php
@@ -3,4 +3,7 @@
 	$email = $_POST['email'];
 	$subject = $_POST['subject'];
 	$message = $_POST['message'];
+	mail($email, $subject, $message, "From:".$from);
+	print "Your message has been sent: </br>$email</br>$subject</br>$message";
+	
 ?>
