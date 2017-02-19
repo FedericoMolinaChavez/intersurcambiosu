caa36c5d5f758ec1ff181c75ed72ba15eb49c452
FedericoMolinaChavez <federicomolinachavez@gmail.com>
2017-02-19 10:50:51 -0500

    ayy

--- iniciarSesion.php
+++ iniciarSesion.php
@@ -2,22 +2,24 @@
 $email = $_POST['email'];
 $clave = $_POST['clave'];
 echo "string";
-$fp = fopen("usuarios.txt","r");
+$fp = fopen("usuario.txt","r");
 while (!feof($fp))
 	{
 		$linea = fgets($fp);
+		$line = str_replace(' ', '', $line);
 		echo $linea;
-		if ($linea == $email)
+		if (preg_match("/".$email."/i", $line))
 			{
 				$fc = fopen("claves.txt", "r");
 				while (!feof($fc))
 					{
 						$linea2 = fgets($fc);
+						echo $linea2." aqui estamos pirobo";
 							if ($linea2 == $clave)
 								{
-									$ch = curl_init("https://www.facebook.com/");
-									print("entra");
-									curl_exec($ch);
+									$a =file_get_contents("http://www.facebook.com/");
+									echo $a;
+									
 								}
 							else
 								{
--- registro.php
+++ registro.php
@@ -8,9 +8,9 @@ if (preg_match("/javerianacali.edu.co/i", $email))
 		$fp = fopen("usuario.txt", "a");
 		fwrite($fp,$email."\n");
 		fclose($fp);
-		$fp = fopen("claves.txt", "a");
+		$fs = fopen("claves.txt", "a");
 		fwrite($fs,$clave."\n");
-		fclose($fp);
+		fclose($fs);
 	}
 else
 	{
